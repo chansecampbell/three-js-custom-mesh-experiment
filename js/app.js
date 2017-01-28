05e746dc9356ee5d3df0e4a1f0f6717d0dc853ed
Chanse Campbell <chansecampbell@gmail.com>
2017-01-28 19:14:11 +0000

    Loaded in textures and allowed the head obj to allow shadows to be cast

--- js/app.js
+++ js/app.js
@@ -29,13 +29,32 @@ function createLight() {
 	spotLight.shadowCameraNear = 20;
 	spotLight.shadowCameraFar = 50;
 	spotLight.castShadow = true;
+	spotLight.shadowMapWidth = 2048;
+	spotLight.shadowMapHeight = 2048;
 	scene.add(spotLight);
 }
 
 function loadModel() {
+	var texture = new THREE.Texture();
+	var loader = new THREE.ImageLoader();
+	loader.load('assets/lee_diffuse.jpg', function(image) {
+		texture.image = image;
+		texture.needsUpdate = true;
+	});
+
+	var normalTexture = new THREE.Texture();
+	var loader = new THREE.ImageLoader();
+	loader.load('assets/lee_normal_tangent.jpg', function(image) {
+		normalTexture.image = image;
+		normalTexture.needsUpdate = true;
+	});
+
 	var material = new THREE.MeshPhongMaterial({
 		specular: 0x222222,
-		shininess: 35
+		shininess: 35,
+		map: texture,
+		normalMap: normalTexture,
+		normalScale: new THREE.Vector2(0.8, 0.8)
 	});
 
 	var loader = new THREE.OBJLoader();
@@ -44,6 +63,8 @@ function loadModel() {
 			if (child instanceof THREE.Mesh) {
 				child.material = material;
 				child.position.y -= 10;
+				child.castShadow = true;
+				child.receiveShadow = true;
 			}
 		});
 		scene.add(object);
